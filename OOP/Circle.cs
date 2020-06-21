1460a78d95cb7b1f0944d863ddd772fe39ba1802
dotnet290320 <62850561+dotnet290320@users.noreply.github.com>
2020-06-21 21:24:33 +0300

    Add files via upload

---
+++ OOP/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32_210620
+{
+    class Circle : Shape
+    {
+        // public double ;
+        public double _radius;
+
+        public override void CalcArea()
+        {
+            double result = Math.PI * _radius * _radius;
+            _area = result;
+        }
+
+        public override void CalcHekef()
+        {
+            double result = 2 * Math.PI * _radius;
+            _hekef = result;
+        }
+    }
+}
---
+++ OOP/TriangleEqualSides.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32_210620
+{
+    class TriangleEqualSides : Triangle
+    {
+        public override void CalcHekef()
+        {
+            _hekef = _a + _a + _a;
+        }
+    }
+}
